05faaa5b24c15c52b3a4c5e70b27fc5e30b649ca
Zweihander2 <liammcateer59@gmail.com>
2025-04-10 19:48:26 -0400

    the player turns depending on the direction they move in

--- Assets/Scripts/Final_Assignment/Player_Movement.cs
+++ Assets/Scripts/Final_Assignment/Player_Movement.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+SpriteRenderer sr;
     public float speed;
+    void Start() {
+        sr = GetComponent<SpriteRenderer>();
+    }
      void Update()
     {
         //Keyboard controls
         float moving = Input.GetAxis("Horizontal");
+
         //Movement speed, adjustable with speed value and kept smooth across frames with Time.deltaTime
         float moveAmount = moving * speed * Time.deltaTime;
+                sr.flipX = moving < 0;
         //Turns the x value of the player's current location into a float value
         float currentX = transform.position.x;
         //Adds the character's movement speed and the immediate value of x together to ensure the player character doesn't get stuck on collision
